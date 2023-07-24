b61d67f04978de9c15ec6ebc7ebed845d0fe2500
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-07-24 15:38:45 +0200

    added list and for cycle

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-alert("hello world");
+// ** MAIL
+
+// array list of accepted users
+
+const acceptedUsers = [
+  "marco.rossi@gmail.com",
+  "umberto.bianchi@gmail.com",
+  "lorem.ipsum@gmail.com",
+  "alessandro.neri@gmail.com",
+  "admin.dude@gmail.com",
+];
+console.log(acceptedUsers.length);
+
+// prompt user mail
+
+const userMail = prompt("Inserisci la tua mail");
+console.log(userMail);
+
+// check list
+
+for (let i = 0; i < acceptedUsers.length; i++) {
+  console.log(i);
+}
